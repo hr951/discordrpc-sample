b0663006ea5c0568c62430c118556cf566fe4f4e
hr951 <123540607+hr951@users.noreply.github.com>
2023-11-17 07:07:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-// discord-rpcをインポート
 var rpc = require("discord-rpc");
 
-// クライアントIDを設定
-const clientId = "1094581121490104361";
+// set clientId
+const clientId = "1170239764251807784";
 
-// RPCクライアントを作成
+// make client
 const client = new rpc.Client({ transport: "ipc" });
 
-// クライアントが準備できたら
+	const date1 = new Date();
+	const date2 = date1.getFullYear() + "年" + 
+				(date1.getMonth() + 1)  + "月" + 
+				date1.getDate() + "日" + 
+				date1.getHours() + "時" + 
+				date1.getMinutes() + "分" + 
+				date1.getSeconds() + "秒"
+
 client.on("ready", () => {
- client.setActivity({
-  details: "test",
-  state: "てすと",
-  startTimestamp: new Date(),
+  client.setActivity({
 
-  largeImageText: "This is test",
-  largeImageKey: "h-750",
-  smallImageText: "aaaa",
-  smallImageText: "h-750",
+    // text
+    details: "＼(^o^)／ｵﾜﾀ",
+    state: "Playing Solo",
+    startTimestamp: new Date(),
 
-  partyId: "6d70795f-a07a-422f-98b3-a93a09a85aed",
-  partySize: 99,
-  partyMax: 1,
- })
+    // image
+    largeImageText: "Developed by hi_ro951",
+    largeImageKey: "h-750",
+
+    // partysize / partymax
+    partyId: "ae488379-351d-4a4f-ad32-2b9b01c91657",
+    partySize: 1,
+    partyMax: 1,
+
+    // button
+    buttons : [
+      {
+        label : "Lit Link", 
+        url : "https://lit.link/hiro951"
+      },
+      {
+        label : `起動時刻：${date2}`, 
+        url : "https://github.com/hr951"
+      }
+    ]
+  })
 });
 
-// クライアントにログイン
 client.login({ clientId }).catch(console.error);
